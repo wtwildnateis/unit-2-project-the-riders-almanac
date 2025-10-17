dfa2e62d500b51e1978c88912b8dd21fef705ad2
wtwildnateis <wtwildnateis@gmail.com>
2025-10-16 20:04:25 -0500

    Initial commit for backend

---
+++ Backend/riders-almanac/src/main/java/com/ridersalmanac/riders_almanac/RidersAlmanacApplication.java
@@ -0,0 +1,13 @@
+package com.ridersalmanac.riders_almanac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RidersAlmanacApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RidersAlmanacApplication.class, args);
+	}
+
+}
---
+++ Backend/riders-almanac/src/test/java/com/ridersalmanac/riders_almanac/RidersAlmanacApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ridersalmanac.riders_almanac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RidersAlmanacApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
