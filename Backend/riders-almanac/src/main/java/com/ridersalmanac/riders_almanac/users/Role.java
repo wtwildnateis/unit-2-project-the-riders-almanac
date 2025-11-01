706ee6a343987e4c0390d3fc11703838f9009ba6
wtwildnateis <wtwildnateis@gmail.com>
2025-11-01 16:35:01 -0500

    added @entity, getter and settersto Role

--- Backend/riders-almanac/src/main/java/com/ridersalmanac/riders_almanac/users/Role.java
+++ Backend/riders-almanac/src/main/java/com/ridersalmanac/riders_almanac/users/Role.java
@@ -1,6 +1,17 @@
 package com.ridersalmanac.riders_almanac.users;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity @Table(name="roles")
+@Getter
+@Setter
 public class Role {
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Short id;
+
+    @Column(nullable=false, unique=true, length=20)
     private String name; // "USER", "ADMIN", "MOD"
 }
