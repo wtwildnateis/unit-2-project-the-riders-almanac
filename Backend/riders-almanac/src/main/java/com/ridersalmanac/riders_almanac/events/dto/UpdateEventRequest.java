071b0eb2115a0af3d71bc69610615625dc9247ef
wtwildnateis <wtwildnateis@gmail.com>
2025-11-03 19:42:08 -0600

    Dto for updating events is built, should be right, double check

---
+++ Backend/riders-almanac/src/main/java/com/ridersalmanac/riders_almanac/events/dto/UpdateEventRequest.java
@@ -0,0 +1,20 @@
+package com.ridersalmanac.riders_almanac.events.dto;
+
+import java.time.Instant;
+
+public record UpdateEventRequest(
+        String title,
+        String type,
+        String flyer,
+
+        Instant start,
+        Instant end,
+
+        String street,
+        String city,
+        String state,
+        String zip,
+
+        String description,
+        String status // "ACTIVE" or "CANCELLED"
+) {}
